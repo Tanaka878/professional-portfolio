e8cee3a08d58f24071ab34785501a291a81166de
Tanaka878 <musungaretanaka@gmail.com>
2025-03-17 16:22:15 +0200

    setting up the project

---
+++ src/app/NavBar/page.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className='flex gap-3'>
+        <div className='text-white '>
+            Tanaka
+        </div>
+
+        <div>Services </div>
+
+        <div>Skills</div>
+        <div>Works</div>
+        <div>Testimonials</div>
+        <div>Contact</div>
+
+      
+    </div>
+  )
+}
+
+export default NavBar
