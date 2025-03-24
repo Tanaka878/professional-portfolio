3c2a1abec21527eba0b3748342d2f79ea9f48178
Tanaka878 <musungaretanaka@gmail.com>
2025-03-24 16:40:42 +0200

    commit

---
+++ src/app/Skills/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Image from 'next/image'
+
+const Skills = () => {
+  return (
+    <div>
+        <div>
+            <Image src={'/Images/mongo.png'} alt={'Mongo'} height={40} width={40}/>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Skills
